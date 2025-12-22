724eb3fcec63cf4f1816dff844a3bedb373072cd
devDanielFelix <danielfelix72@hotmail.com>
2025-12-22 00:50:14 -0300

    update

--- scripts.js
+++ scripts.js
@@ -40,9 +40,9 @@ document.querySelector(".weather-group").addEventListener('click', function(e) {
     }
 });
 
-
-// Clock Section
-newDate() = getHours();
-newDate() = getMinutes();
-newDate() = getSeconds();
-
+setInterval(function() {
+    let localTime = new Date();
+    document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2, '0');
+    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2, '0');
+    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2, '0');
+}, 1000);
